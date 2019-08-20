b1e148fa9dcd736bd6f139038f43298d140386c9
Chris Junker <elevatemedfit@gmail.com>
2019-08-20 18:53:27 +0000

    Done.

--- db/schema_migration.sql
+++ db/schema_migration.sql
@@ -4,5 +4,5 @@
 CREATE TABLE pokemon(
   id INTEGER PRIMARY KEY,
   name TEXT,
-  type Text
+  type TEXT
 );
--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -10,6 +10,19 @@ class Pokemon
   end
 
   def self.save(name, type, db)
-      db.execute( "INSERT INTO pokemon ( name, type ) VALUES ( ?, ? )", [name, type])
+      db.execute( "INSERT INTO pokemon ( name, type ) VALUES ( ?, ? )", [name, type])#Instead, you should use bound parameters for any variable data that you are passing into your SQL statement:
   end
+
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    type = row[2]
+    self.new(id, name, type)
+  end
+
+def self.find(id, db)
+  result = db.execute("SELECT * FROM pokemon WHERE id = ?;", id).flatten
+  Pokemon.new(id: result[0], name: result[1], type: result[2], db: db)
+end
+
 end
