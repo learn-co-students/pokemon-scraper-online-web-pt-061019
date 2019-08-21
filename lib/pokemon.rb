f34cf3aa077f07d87044e4759cc2b5ffc08e9512
David Foote <david.a.foote@gmail.com>
2019-08-21 23:28:54 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,11 @@
 class Pokemon
+  # db.execute( "INSERT INTO Products ( stockID, Name ) VALUES ( ?, ? )", [id, name])
+  attr_accessor :id, :name, :type, :db
+  
+  def initialize(pokemon) #keyword arguments
+    @id = pokemon[id]
+    @name = pokemon[name]
+    @type = pokemon[type]
+    @db = pokemon[db]
+  end
 end
