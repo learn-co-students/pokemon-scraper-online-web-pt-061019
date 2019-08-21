f3a0601ed9f2edfcf40262eb98a9b91465cda5e0
ennalezah <hae.villareal@gmail.com>
2019-08-21 00:28:19 -0700

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,9 +3,9 @@ class Scraper
 
   def initialize(db)
     self.db = db
-    self.file = File.open("pokemon_index.html")
-    self.parsed_file = Nokogiri::HTML.parse(file)
-    self.all_pokemon = catch_em_all
+    self.file = File.open("pokemon_index.html")   #gets html of pg
+    self.parsed_file = Nokogiri::HTML.parse(file) #nokogiri doc
+    self.all_pokemon = catch_em_all               #container of all pk info in doc
   end
 
   def catch_em_all
@@ -13,18 +13,18 @@ class Scraper
   end
 
   def get_pokemon_name_from(node)
-    node.css(".ent-name").text
+    node.css(".ent-name").text   #gets pk name
   end
 
   def get_pokemon_type_from(node)
-    node.css(".itype").text
+    node.css(".itype").text      #gets pk type
   end
 
   def scrape
-    all_pokemon.each do |pk_node|
-      pk_name = get_pokemon_name_from(pk_node)
-      pk_type = get_pokemon_type_from(pk_node)
-      Pokemon.save(pk_name, pk_type, db)
+    all_pokemon.each do |pk_node|                #iterates through all pk info
+      pk_name = get_pokemon_name_from(pk_node)   #sets pk name
+      pk_type = get_pokemon_type_from(pk_node)   #sets pk type
+      Pokemon.save(pk_name, pk_type, db)         #saves specific pk to db
     end
   end
 
