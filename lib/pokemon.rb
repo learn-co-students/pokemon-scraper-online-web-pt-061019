76e5cdb7b4ec3d0c8cd7f9690d954c30a6fb0605
David Foote <david.a.foote@gmail.com>
2019-08-22 03:37:25 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -24,12 +24,12 @@ class Pokemon
   end
   
   def self.save(name, type, db)  #parameterized query
-    db.execute("INSERT INTO pokemon (name, type) VALUES ( ?, ?)", name, type)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
   end
   
   def self.find(id, db)
     result = db.execute("SELECT * FROM pokemon WHERE id = ?", id)[0]
-  Pokemon.new(id: result[0], name: result[1], type: result[2], db: db)
+    Pokemon.new(id: result[0], name: result[1], type: result[2], db: db)
   # Pokemon.new(result)
   
  end
