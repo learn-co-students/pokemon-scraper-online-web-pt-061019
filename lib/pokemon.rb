856fde491951793f447d38101ab0fc2779b01df5
Christian Polanco <chrispolanco@sbcglobal.net>
2019-08-29 02:08:32 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -9,19 +9,28 @@ class Pokemon
     @name = name
     @type = type
     @db = db
+ 
   end
   
-
-  def save 
+  
+  def self.save (name, type, db)
     sql = <<-SQL
-      INSERT INTO pokemon (name, grade) 
+      INSERT INTO pokemon (name, type) 
       VALUES (?, ?)
     SQL
 
-    DB[:conn].execute(sql, self.name, self.type)
+    db.execute(sql, name, type)
 
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
   end 
+  
+  def self.find(id, db) 
+    sql= "SELECT * FROM pokemon WHERE id = ?"
+    result = db.execute(sql, id)[0]
+    Pokemon.new(id: result[0],name: result[1],type: result[2], db: db)
+    
+  end 
+  
   
   
 end
